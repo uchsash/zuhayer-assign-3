167e2195222a8e1b20976c80d1c8847c5e7be1a2
uchsash <uchsash.tousif@gmail.com>
2026-01-04 19:39:41 +0600

    queries.sql with four queries is uploaded.

---
+++ queries.sql
@@ -0,0 +1,46 @@
+-- Query 1
+select
+    b.*,
+    u.name,
+    v.vehicle_name
+from
+    bookings as b
+    inner join users as u on b.user_id = u.id
+    inner join vehicles as v on b.vehicle_id = v.id;
+
+-- Query 2
+SELECT
+    *
+FROM
+    Vehicles v
+WHERE
+    NOT EXISTS (
+        SELECT
+            *
+        FROM
+            Bookings b
+        WHERE
+            b.vehicle_id = v.id
+    );
+
+-- Query 3
+select
+    *
+from
+    vehicles
+where
+    type = 'car'
+    and availability_status = 'available';
+
+-- Query 4
+select
+    v.vehicle_name,
+    count(*) as total_bookings
+from
+    bookings as b
+    join vehicles as v on b.vehicle_id = v.id
+group by
+    v.id,
+    v.vehicle_name
+having
+    count(*) > 2;
