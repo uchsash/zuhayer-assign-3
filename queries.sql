11e56c5760237811d0d901aee6c057713b0e9d7a
uchsash <uchsash.tousif@gmail.com>
2026-01-04 20:37:16 +0600

    query 1 updated as expected result.

--- queries.sql
+++ queries.sql
@@ -1,8 +1,11 @@
 -- Query 1
 select
-    b.*,
-    u.name,
-    v.vehicle_name
+    b.booking_id,
+    u.name as customer_name,
+    v.vehicle_name,
+    b.rent_start_date,
+    b.rent_end_date,
+    b.booking_status
 from
     bookings as b
     inner join users as u on b.user_id = u.id
